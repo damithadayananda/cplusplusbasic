764e795ec37f7f030db5216efc263c89d079b50d
damitha <damitha@pickme.lk>
2019-10-29 16:15:12 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+void helloworld();
+int main()
+{
+    cout << "Hello world!" << endl;
+    helloworld();
+    return 0;
+}
+void helloworld(){
+    cout << "in function" << endl;
+}
